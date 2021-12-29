baeb79fd346cd4526e41c7e3bd1b26dc2032a190
Diego Ramos <diego_ramos93@outlook.com>
2021-12-29 17:02:20 -0300

    alguns ajustes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,7 @@ function About(){
             <div>
                 <img src={team} alt="Equipe" className={styles.about_img}/>
             </div>
-            <p className={styles.about_us}>Estamos a anos no mercado ajudando cada vez mais empresas a crecer, nós acreditamos que melhor que contratar ótimos profissionais é treina-los, levando assim cada vez mais pessoas a ter um futuro brilhante, com esse pensamento fazemos cada vez mais empresas pequenas se tornarem grandes, venha nos conhecer e veja que juntos somos mais fortes!</p>
+            <p className={styles.about_us}>Estamos a anos no mercado ajudando cada vez mais empresas a crescer, nós acreditamos que melhor que contratar ótimos profissionais é treina-los, levando assim cada vez mais pessoas a ter um futuro brilhante, com esse pensamento fazemos cada vez mais empresas pequenas se tornarem grandes, venha nos conhecer e veja que juntos somos mais fortes!</p>
             </section>
 
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -43,7 +43,7 @@ function Home() {
             <MDBCarouselCaption>
               <h5>Gestão de E-Comerce</h5>
               <p>Somos especialistas em e-comerce podemos te ajudar desde a implementação até a manutenção</p>
-              <button className={styles.main_btn}>Simule</button>
+              <button  className={styles.main_btn}>Simule</button>
             </MDBCarouselCaption>
           </MDBCarouselItem>
         </MDBCarouselInner>
@@ -58,42 +58,42 @@ function Home() {
       <Container>
         <Row className={styles.homeGrid1}>
           <Col className={styles.gridItem}>
-          <a href="#"><img src={grid1} alt="Análise de negócios" /></a><br />
-          <a href="#"> <p>Análise de mercado</p></a><br />
+          <a href="/contact"><img src={grid1} alt="Análise de negócios" /></a><br />
+          <a href="/contact"> <p>Análise de mercado</p></a><br />
           <span>Melhore os resultados e ganhe agilidade nos negócios com ações baseadas em dados e análises.</span><br />
-          <a href="#">Saiba mais</a>
+          <a href="">Saiba mais</a>
           </Col>
           <Col className={styles.gridItem}>
-          <a href="#"><img src={grid2} alt="Estratégia de cliente" /></a><br />
-          <a href="#"> <p>Estratégia de cliente</p></a><br />
+          <a href="/contact"><img src={grid2} alt="Estratégia de cliente" /></a><br />
+          <a href="/contact"> <p>Estratégia de cliente</p></a><br />
           <span>Conheça seu cliente, seus comportamentos, desejos e espectativas, com uma estratégia testada e aprovada.</span><br />
-          <a href="#">Saiba mais</a>
+          <a href="/contact">Saiba mais</a>
           </Col>
           <Col className={styles.gridItem}>
-          <a href="#"><img src={grid3} alt="e-comerce" /></a><br />
-          <a href="#"> <p>Integração ao e-comerce</p></a><br />
+          <a href="/contact"><img src={grid3} alt="e-comerce" /></a><br />
+          <a href="/contact"> <p>Integração ao e-comerce</p></a><br />
           <span>Ajudamos sua empresa em todas as etapas da integração até manutenção do principal meio de negócios atualmente, o e-comerce.</span><br />
-          <a href="#">Saiba mais</a>
+          <a href="/contact">Saiba mais</a>
           </Col>
         </Row>
         <Row className={styles.homeGrid1}>
           <Col className={styles.gridItem}>
-          <a href="#"><img src={grid4} alt="Treinamento profissional" /></a><br />
-          <a href="#"> <p>Treinamento profissional</p></a><br />
+          <a href="/contact"><img src={grid4} alt="Treinamento profissional" /></a><br />
+          <a href="/contact"> <p>Treinamento profissional</p></a><br />
           <span>Tenha acesso aos melhores cursos do mercado para toda sua equipe.</span><br />
-          <a href="#">Saiba mais</a>
+          <a href="/contact">Saiba mais</a>
           </Col>
           <Col className={styles.gridItem}>
-          <a href="#"><img src={grid5} alt="Gestão de processos" /></a><br /> 
-          <a href="#"> <p>Gestão de processos</p></a><br />
+          <a href="/contact"><img src={grid5} alt="Gestão de processos" /></a><br /> 
+          <a href="/contact"> <p>Gestão de processos</p></a><br />
           <span>Nosso objetivo é proporcionar uma completa e abrangente melhoria operacional e financeira.</span><br />
-          <a href="#">Saiba mais</a>
+          <a href="/contact">Saiba mais</a>
           </Col>
           <Col className={styles.gridItem}>
-          <a href="#"><img src={grid6} alt="Produtividade e otimização" /></a><br />
-          <a href="#"> <p>Produtividade e otimização</p></a><br />
+          <a href="/contact"><img src={grid6} alt="Produtividade e otimização" /></a><br />
+          <a href="/contact"> <p>Produtividade e otimização</p></a><br />
           <span>Atuamos em todas as áreas do negócio garantindo um ambiente de crescimento e ganhos para todos.</span><br />
-          <a href="#">Saiba mais</a>
+          <a href="/contact">Saiba mais</a>
           </Col>
         </Row>
       </Container>
@@ -106,16 +106,19 @@ function Home() {
       <h2>O que é uma consultoria empresarial:</h2>
       <p>
       Esse tipo de serviço funciona como um diagnóstico de problemas e análise do cenário e dos dados de uma empresa. Seu objetivo é trazer soluções para tornar a gestão competitiva e eficiente, melhorando a produtividade, a tomada de decisão e o crescimento e possibilitando redução de custos, eficiência nos processos, otimização de finanças e muitas outras coisas.</p>
+      <a href="/contact">Saiba mais</a>
 
   <h3>Qual a importância da consultoria empresarial?</h3>
   <p>
         Se o empreendedor quer fazer seu negócio crescer e alcançar resultados melhores, então a consultoria empresarial é a ferramenta ideal para ele. O processo de consultoria é fundamental para identificar falhas, setores que necessitam de melhorias e para explorar o potencial da equipe.
         </p>
+        <a href="/contact">Saiba mais</a>
 
         <h4>Como funciona a consultoria empresarial?</h4>
         <p>
         Este é um processo de parceria entre gestor e consultor, que envolve reuniões para a compreensão das demandas apresentadas e da troca de experiências relevantes sobre a realidade do negócio. Além disso, existe uma etapa de análise de dados para o diagnóstico do negócio. 
         </p>
+        <a href="/contact">Saiba mais</a>
       </Container>
 
       {/*End of Body Text*/}
